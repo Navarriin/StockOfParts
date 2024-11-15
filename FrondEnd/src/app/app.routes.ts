cd1621e5e522208556d1d5263298d465c3525c91
Gabriel <gabrielnavarro.vip@gmail.com>
2024-11-15 16:18:43 -0300

    criação de rotas e navbar

--- FrondEnd/src/app/app.routes.ts
+++ FrondEnd/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((page) => page.HomeComponent),
+  },
+  {
+    path: 'list',
+    loadComponent: () =>
+      import('./pages/list/list.component').then((page) => page.ListComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
