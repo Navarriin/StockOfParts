5ab060793e31288251abaf1b1385d2262dfabe75
Gabriel <gabrielnavarro.vip@gmail.com>
2024-11-15 13:44:38 -0300

    Criando esqueleto do FrontEnd

--- BackEnd/Dtos/PartsResponseDto.cs
+++ BackEnd/Dtos/PartsResponseDto.cs
@@ -4,8 +4,6 @@ namespace StockOfMachineParts.Dtos;
 
 public record PartsResponseDto(int Id, string PartsName, string MachineName, int Quantity, decimal Price);
 
-public record MachineResponseDto(int Id, string ModelName);
-
 public static class PartsResponseMapper
 {
     public static PartsResponseDto MapToPartsResponseDto(Parts parts) =>
